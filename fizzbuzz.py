1cd76f0cc947b7e025e164d9e4e66dde8e511ad1
sohrabmoradi <sohrab.mk1990@yahoo.com>
2025-02-01 09:40:42 -0330

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+# FizzBuzz program
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+# Run the FizzBuzz program
+fizz_buzz()
